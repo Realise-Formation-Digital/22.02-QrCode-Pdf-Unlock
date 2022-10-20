73376f1e11a5cac7f0b6e0e1197b46a7116c67cf
Marco Tribuzio <marco.tribuz@gmail.com>
2022-10-20 16:12:09 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const pdfUnlock = require('./routes/pdfUnlock');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload({
+  createParentPath: true,
+  limits: {
+    fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
+  },
+}));
+app.use('/pdfUnlock', pdfUnlock);
+
+module.exports = app;
---
+++ routes/pdfUnlock.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const pdftk = require('node-pdftk');
+
+router.post('/', async function(req, res) {
+  try{
+    if(!req.files) res.send({
+        status: false,
+        message: 'No file uploaded'
+      });
+
+    //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+    let pdf = req.files.pdf;
+
+    const test = await pdftk.input(pdf.data).output();
+
+    res.contentType("application/pdf");
+    res.end(new Buffer(test, 'binary'), 'binary');
+
+  }catch (e) {
+    console.error('[API][pdfUnlock][pdfUnlock] An error has occurred when unlocking pdf', e)
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
